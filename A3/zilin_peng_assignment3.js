0f9ae8330d9eca352dc0e20290ba29205a1e87fa
ZilinPeng <rzlpeng@ucdavis.edu>
2022-07-20 16:46:27 -0700

    A3

--- A3/zilin_peng_assignment3.js
+++ A3/zilin_peng_assignment3.js
@@ -28,7 +28,36 @@ const tableInfor = {
   ],
 };
 
+let body = document.getElementById("body");
+
+let table = document.createElement("table");
+let thead = document.createElement("thead");
+let tbody = document.createElement("tbody");
+
+table.appendChild(thead);
+table.appendChild(tbody);
+
+body.appendChild(table);
+
+let row = document.createElement("tr");
+tableInfor.tableHeader.forEach((ele) => {
+  let thead = document.createElement("th");
+  thead.innerHTML(ele);
+  row.appendChild(thead);
+});
+thead.appendChild(row);
+
 const list = ["HTML", "JavaScript", "CSS", "React", "Redux", "Java"];
+let ul = document.createElement("ul");
+let ol = document.createElement("ol");
+list.forEach((ele) => {
+  let li = createElement("li");
+  li.innerHTML = ele;
+  ul.appendChild(li);
+  ol.appendChild(li);
+});
+body.appendChild(ul);
+body.appendChild(ol);
 
 const dropDownList = [
   { value: "newark", content: "Newark" },
@@ -38,3 +67,13 @@ const dropDownList = [
   { value: "dalyCity", content: "Daly City" },
   { value: "sanJose", content: "San Jose" },
 ];
+let select = document.createElement("select");
+select.name = "city";
+select.id = "city";
+dropDownList.forEach((ele) => {
+  let option = document.createElement("option");
+  option.value = ele.value;
+  option.text = ele.content;
+  select.appendChild(option);
+});
+body.appendChild(select);
